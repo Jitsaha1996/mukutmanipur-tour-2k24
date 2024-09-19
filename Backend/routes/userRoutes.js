a814d3b10b5da5e680ec3fd9f5a4b48effc7f2ac
Jitsaha1996 <sahajit1996@gmail.com>
2024-09-19 09:18:41 +0530

    cache build

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,9 +1,17 @@
 const express =require("express")
 const router=express.Router();
+const app = express();
 const {registerUsers, authUsers,getUserList,editUsers}=require("../controllers/userController")
 router.route('/').post(registerUsers);
+
 router.route('/').get(getUserList);
 router.route('/login').post(authUsers);
 router.route('/edit').put(editUsers);
+app.use(express.static(path.join(__dirname, '..', '..', 'Web', 'build'))); // Adjust path to the build folder
+
+// Catch-all route for any other request
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '..', 'Web', 'build', 'index.html')); // Adjust path to index.html
+});
 
 module.exports=router;
